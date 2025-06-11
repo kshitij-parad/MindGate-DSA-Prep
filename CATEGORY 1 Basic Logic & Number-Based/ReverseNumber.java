2f17e49766de1d93923be3a4363bc0f29140cb30
kshitij-parad <kshitijparad964@gmail.com>
2025-06-11 20:45:59 +0530

    Reversed Number

--- CATEGORY 1 Basic Logic & Number-Based/ReverseNumber.java
+++ CATEGORY 1 Basic Logic & Number-Based/ReverseNumber.java
@@ -1,6 +1,17 @@
 class ReverseNumber{
     public static void main(String[] args){
 
+        int rev =0;
+        int num = 1022;
+        int digit;
+
+        while(num > 0){
+            digit = num %10;
+            rev = rev * 10 + digit;
+            num = num /10; 
+        }
+
+        System.out.println(rev);
       
     }
 }
