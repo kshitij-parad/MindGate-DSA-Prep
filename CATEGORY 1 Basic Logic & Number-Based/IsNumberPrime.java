3df8abbbf0a76d17a71caa41735a681668084128
kshitij-parad <kshitijparad964@gmail.com>
2025-06-11 22:12:19 +0530

    Is Number Prime

---
+++ CATEGORY 1 Basic Logic & Number-Based/IsNumberPrime.java
@@ -0,0 +1,26 @@
+public class IsNumberPrime {
+
+    static boolean isPrime(int num){
+
+        if(num == 1) return false;
+
+
+        //--Correct but less optimal
+
+        // for(int i = 2; i< num -1 ; i++){
+        //     if(num % i == 0) return  false;
+        // }
+
+        //More optimal
+        for(int i = 2; i< Math.sqrt(num) ; i++){
+            if(num % i == 0) return  false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        boolean res = IsNumberPrime.isPrime(11);
+        System.out.println(res);
+    }
+}
