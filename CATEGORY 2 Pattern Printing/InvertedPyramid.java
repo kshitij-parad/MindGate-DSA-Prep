2eb34ea82feb0f777a175da1932b6e4d238ccfc8
kshitij-parad <kshitijparad964@gmail.com>
2025-06-12 16:01:04 +0530

    Inverted Pyramid

---
+++ CATEGORY 2 Pattern Printing/InvertedPyramid.java
@@ -0,0 +1,24 @@
+
+public class InvertedPyramid {
+
+    static void  invertedPyramid(int n){
+
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+        
+        for(int i = n ; i >= 1 ; i--){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        InvertedPyramid.invertedPyramid(5);
+    }
+}
