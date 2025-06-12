8faaacadb162313de69a6343d398767d110cbdf9
kshitij-parad <kshitijparad964@gmail.com>
2025-06-12 15:44:02 +0530

    Sum Of Digit In Number

--- CATEGORY 1 Basic Logic & Number-Based/SumOfDigitInNumber.java
+++ CATEGORY 1 Basic Logic & Number-Based/SumOfDigitInNumber.java
@@ -1,3 +1,19 @@
 public class SumOfDigitInNumber {
-    
+
+    static int sumOfDigitInNumber(int num){
+
+        int res = 0;
+
+        while(num  >0 ){
+            int digit = num % 10;
+            res += digit;
+            num /=10; 
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(SumOfDigitInNumber.sumOfDigitInNumber(101));
+
+    }
 }
