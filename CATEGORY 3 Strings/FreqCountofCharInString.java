d5b4e7c1df81b5946806ade3659777da9b5fce75
kshitij-parad <kshitijparad964@gmail.com>
2025-06-13 21:34:37 +0530

    Frequency count of character in String

--- CATEGORY 3 Strings/FreqCountofCharInString.java
+++ CATEGORY 3 Strings/FreqCountofCharInString.java
@@ -1,3 +1,23 @@
+
+import java.util.LinkedHashMap;
+
 public class FreqCountofCharInString {
-    
+
+    static LinkedHashMap<Character, Integer> freqChar(String str) {
+
+        LinkedHashMap<Character, Integer> hm = new LinkedHashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (hm.containsKey(str.charAt(i))) {
+                hm.put(str.charAt(i), hm.get(str.charAt(i)) + 1);
+            } else {
+                hm.put(str.charAt(i), 1);
+            }
+        }
+        return hm;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FreqCountofCharInString.freqChar("banana"));
+    }
 }
