8ed1e0ad8c86234f3991aacfdc7280c91e541cda
kshitij-parad <kshitijparad964@gmail.com>
2025-06-14 13:16:39 +0530

    Frequecy Count Core Logic

---
+++ CATEGORY 4 Arrays/FrequecyCountCoreLogic.java
@@ -0,0 +1,31 @@
+public class FrequecyCountCoreLogic {
+
+    static void freqCount(int[] arr) {
+        boolean[] visited = new boolean[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (visited[i]) {
+                continue;
+            }
+
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.printf("%d appeared %d times. ", arr[i], count);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 1, 4};
+        FrequecyCountCoreLogic.freqCount(arr);
+    }
+}
