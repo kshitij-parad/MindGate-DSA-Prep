1a0ebe955c16b0ab9640555c64afa127e3af1c3a
kshitij-parad <kshitijparad964@gmail.com>
2025-06-14 17:11:45 +0530

    Bubble sort

--- CATEGORY 4 Arrays/BubbleSort.java
+++ CATEGORY 4 Arrays/BubbleSort.java
@@ -1,5 +1,21 @@
-package CATEGORY 4 Arrays;
-
 public class BubbleSort {
-    
+    public static void main(String[] args) {
+        
+        int[] arr  = {1,20,3,3,4,0,95};
+        int temp;
+        for(int i = 0 ; i< arr.length-1 ; i++){
+            for(int j =0 ; j < arr.length-1-i ; j++ ){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for(int n : arr){
+            System.out.println(n);
+        }
+
+    }
 }
