58c99b7da750491ecb1280504741234076d6016f
kshitij-parad <kshitijparad964@gmail.com>
2025-06-18 18:31:12 +0530

    Bubble sort optmized if aray is already sorted O(n)

--- Searching Sorting/BubbleSort.java
+++ Searching Sorting/BubbleSort.java
@@ -6,15 +6,19 @@ public class BubbleSort {
         //2,18,2,3,6,1
 
         for(int i = 0 ; i < arr.length-1;i++){
+            boolean swapped = false;
             for(int j = 0 ; j < arr.length-1-i; j++){
                 
                 if(arr[j] > arr[j+1]){
                     temp = arr[j];
                      arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
 
             }
+
+            if(!swapped) break;
         }
     }
 
