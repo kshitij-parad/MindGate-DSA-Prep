db38703746ea8289498afec2e606da279257f072
kshitij-parad <kshitijparad964@gmail.com>
2025-06-18 18:29:48 +0530

    bublle sort

---
+++ Searching Sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+
+    static void bubbleSort(int[] arr){
+        int temp;
+
+        //2,18,2,3,6,1
+
+        for(int i = 0 ; i < arr.length-1;i++){
+            for(int j = 0 ; j < arr.length-1-i; j++){
+                
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                     arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,18,2,3,6,1};
+
+        BubbleSort.bubbleSort(arr);
+
+        for(int e : arr)
+            System.out.println(e);
+    }
+}
